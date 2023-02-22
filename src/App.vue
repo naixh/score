6f26ad8199cdf31103a2777a043d37b96ad28013
naish <naish@hummingbird.travel>
2023-02-22 14:16:49 +0500

    done

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
     <div class="h-screen">
         <div class="bg-white-800 h-full">
             <div class="flex bg-red-300 flex-row sm:h-1/2">
-                <div class="grow w-full  bg-blue-800  place-items-center grid lg:visible md:visible ">
+                <div class="grow w-7/12  bg-blue-800  place-items-center grid lg:visible md:visible ">
                     <div class="align-middle items-center text-center invisible lg:visible md:visible">
                         <p class=" text-9xl text-white"> FINAL SCORE </p>
                         <br>
@@ -12,13 +12,13 @@
                     <div class="grow w-1/2 h-full bg-blue-800   text-center grid place-content-evenly ">
                         <div class="align-middle items-center text-center">
                             <textarea spellcheck="false" class=" sm:w-full sm:text-8xl align-bottom font-extrabold text-white bg-transparent text-center" >TEAM</textarea>
-                            <input class="w-full align-middle text-white bg-transparent text-center sm:font-extrabold " style="font-size: 12.75rem;" type="number">
+                            <input v-model="f_score_in"  class="w-full align-middle text-white bg-transparent text-center sm:font-extrabold " style="font-size: 12.75rem;" type="number">
                         </div>
                     </div>
                     <div class="grow-0 w-1/2  bg-blue-800 text-center grid place-content-evenly">
                         <div class="align-middle items-center text-center">
                             <textarea spellcheck="false" class=" sm:w-full sm:text-8xl align-bottom font-extrabold text-white bg-transparent text-center" >TEAM</textarea>
-                            <input class="w-full align-middle text-white bg-transparent text-center font-extrabold " style="font-size: 12.75rem;" type="number">
+                            <input v-model="f_score_out" class="w-full align-middle text-white bg-transparent text-center font-extrabold " style="font-size: 12.75rem;" type="number">
                         </div>
                     </div>
                 </div>
@@ -27,7 +27,7 @@
                 <div class="flex bg-red-300 flex-row h-full">
                     <div class="grow w-7/12 bg-gray-900  grid  border-r-8">
                         <div class="align-middle text-center">
-                            <p class=" text-8xl align-top text-red-600"> Time </p>
+                            <p class=" text-8xl align-top font-bold text-red-600"> Time </p>
                             <br>
                             <div id="counter" v-cloak>
                                 <div v-if="time!==0">
@@ -51,30 +51,23 @@
                     </div>
                     <div class="grow-0 w-full bg-gray-900">
                         <div class="grow w-full h-4/6 bg-gray-200 flex">
-                            <div class="grow w-1/3 h-full bg-gray-900 p-5  ">
-                                <input type="number" class="bg-yellow-300 rounded-lg  text-9xl font-bold w-full text-center" style="font-size: 12rem;"  >
+                            <div class="grow w-1/4 h-full bg-gray-900 p-5  ">
+                                <input type="number" v-model="p_score_in" class="bg-yellow-300 rounded-lg  text-9xl font-bold w-full text-center" style="font-size: 16rem;"  >
 
                             </div>
-                            <div class="grow-0 w-1/2 h-full bg-gray-900 ">
-                                <div class="bg-gray-900 h-full text-center grid p-20">
+                            <div class="grow-0 w-1/3 h-full bg-gray-900 ">
+                                <div class="bg-gray-900 h-full text-center grid p-20  grid place-content-evenly">
                                     <span class="text-8xl font-bold text-white whitespace-normal  " >Period Score</span>
                                 </div>
                             </div>
-                            <div class="grow-0 w-1/3  bg-gray-900">
+                            <div class="grow-0 w-1/4  bg-gray-900">
                                 <div class="grow h-full bg-gray-900 p-5  ">
-                                    <input type="number" class="bg-yellow-300 rounded-lg  text-9xl font-bold w-full text-center" style="font-size: 12rem;"  >
+                                    <input type="number"  v-model="p_score_out" class="bg-yellow-300 rounded-lg  text-9xl font-bold w-full text-center" style="font-size: 16rem;"  >
 
                                 </div>
                             </div>
                         </div>
-                        <div class="grow-0 w-full  h-1/3 bg-gray-900 text-center ">
-                            <div class="grow h-full w-5/12 bg-gray-900 float-left grid place-content-evenly pt-2">
-                                <div class="bg-gray-900  h-full text-white">
-                                    <span class="text-6xl font-bold " >CATCH</span>
-                                    <input type="number" class="bg-gray-900 text-8xl font-bold w-full text-center" >
-                                </div>
-                            </div>
-                        </div>
+
 
                     </div>
                 </div>
@@ -87,26 +80,30 @@
 <script>
 
 
+import { toHandlers } from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
     components: {
         'timer':HelloWorld
     },
-data() {
-    return {
-        componentLoaded: false,
-        isRunning: false,
-        minutes: 10,
-        secondes: 10,
-        time:0,
-        min: 0,
-        sec: 0,
-        timer: null,
-        sound: new Audio("http://s1download-universal-soundbank.com/wav/nudge.wav")
-    }
-},
-
+    data() {
+        return {
+            componentLoaded: false,
+            isRunning: false,
+            minutes: 0,
+            secondes: 0,
+            p_score_in:0,
+            p_score_out:0,
+            f_score_in:0,
+            f_score_out:0,
+            time:0,
+            min: 0,
+            sec: 0,
+            timer: null,
+            sound: new Audio("http://s1download-universal-soundbank.com/wav/nudge.wav")
+        }
+    },
 
 	methods: {
 		 start () {
@@ -139,10 +136,49 @@ data() {
             this.time = (this.min * 60 + this.secondes)
             this.time = this.prettify(this.time)
 		 },
+         keyDownHandler(e) {
+        console.log(e.key)
+        // Your handler code here
+        },
+    },
+    
+    created() {
+    window.addEventListener('keydown', (e) => {
+        console.log(e.key)
+        if (e.key == 'ArrowLeft') {
+            e.preventDefault()
+             this.p_score_in++
+             this.f_score_in++
+        }
+        if (e.key == 'ArrowRight') {
+            e.preventDefault()
+             this.p_score_out++
+             this.f_score_out ++
+        }
 
+        if (e.key == 'ArrowUp') {
+            e.preventDefault()
+             this.p_score_in--
+             this.f_score_in--
+        }
+        if (e.key == 'ArrowDown') {
+            e.preventDefault()
+             this.p_score_out--
+             this.f_score_out--
+        }
 
-	},
-
+        if (e.key == 'Alt') {
+            if(this.isRunning == false){
+                this.start()
+            }else{
+                this.stop()
+            }
+           
+        }
+    });
+  }
+    
+  
 }
 
 </script>
